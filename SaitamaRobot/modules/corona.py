9ccffa70a586a54d85bbf0f30b3c8d675a444951
NOOB <68851382+sipakisking@users.noreply.github.com>
2020-10-04 08:04:33 +0530

    Update corona.py

--- SaitamaRobot/modules/corona.py
+++ SaitamaRobot/modules/corona.py
@@ -1,5 +1,5 @@
 import random
-from telegram.ext import run_async, Filters
+from telegram.ext import CallbackContext, run_async, Filters
 from telegram import Message, Chat, Update, Bot, MessageEntity
 from SaitamaRobot import dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
@@ -17,8 +17,8 @@ SFW_STRINGS = (
   )
 
 @run_async
-def corona(bot: Bot, update: Update):
-    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+def corona(update: Update, context: CallbackContext):
+    context.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
     message = update.effective_message
     if message.reply_to_message:
       message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
